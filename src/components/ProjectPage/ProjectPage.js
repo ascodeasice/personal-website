99fd9c1dd39b63aa2662792a680ca3ce0169b2e7
ascodeasice <ab0988956087@gmail.com>
2023-03-02 11:05:17 +0800

    Fix padding problem

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -4,7 +4,7 @@ import ProjectInfo from "./ProjectInfo";
 const ProjectPage = ({ project }) => {
 
     return (
-        <div className="h-screen bg-black grid justify-items-center pt-[15vh]">
+        <div className="h-[90vh] bg-black grid justify-items-center pt-[10vh]">
             <div className="w-[80vw] grid grid-cols-2 grid-rows-[repeat(4,min-content)] mt-[10vh]">
                 <h2 className="text-beige col-span-2 text-4xl font-bold mb-4">{project.name}</h2>
                 <ProjectImage src={project.image} />
