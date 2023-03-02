36e33ea744523ea07c8de794b73b050b6ae853be
ascodeasice <ab0988956087@gmail.com>
2023-03-02 11:23:26 +0800

    Add contact translation in contact page

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -2,13 +2,15 @@ import ContactRow from "./ContactRow";
 import GITHUB from "../../assets/icons/github.svg";
 import LINKEDIN from "../../assets/icons/linkedin.svg";
 import MAIL from "../../assets/icons/mail.svg";
-
+import { useTranslation } from "react-i18next";
 
 const ContactPage = () => {
+    const { t } = useTranslation();
+
     return (
         <div id="contactScroll" className="contactTrigger h-screen bg-black grid  pt-[15vh] justify-items-center">
             <div className="grid grid-rows-[min-content_1fr] gap-y-4 w-[70vw]">
-                <h1 className="text-beige font-bold text-6xl justify-self-start">Contact</h1>
+                <h1 className="text-beige font-bold text-6xl justify-self-start">{t("Contact")}</h1>
                 <div className="bg-beige grid grid-rows-3 h-[60vh] p-8">
                     <ContactRow src={GITHUB} href="#" text="Github" />
                     <ContactRow src={LINKEDIN} href="#" text="LinkedIn" />
