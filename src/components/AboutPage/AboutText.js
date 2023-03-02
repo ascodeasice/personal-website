8ffe8f6f8fd536eb74ccdb3461b0915384f1f877
ascodeasice <ab0988956087@gmail.com>
2023-03-02 13:45:02 +0800

    Add more text in about page

--- src/components/AboutPage/AboutText.js
+++ src/components/AboutPage/AboutText.js
@@ -4,10 +4,10 @@ const AboutText = () => {
     const { t } = useTranslation();
     return (
         // TODO: use grid to space everything evenly
-        <div className="bg-maroon rounded-lg py-4 px-4 max-w-[50vw] h-[70vh] grid grid-rows-3 gap-y-4">
-            <p className="text-beige whitespace-pre-wrap text-xl">{t("aboutPara1")}</p>
-            <p className="text-beige whitespace-pre-wrap text-xl">{t("aboutPara2")}</p>
-            <p className="text-beige whitespace-pre-wrap text-xl">{t("aboutPara3")}</p>
+        <div className="bg-maroon rounded-lg py-4 px-4 max-w-[50vw] h-[70vh] grid grid-rows-[repeat(3,min-content)] gap-y-4">
+            <p className="text-beige whitespace-pre-wrap text-lg">{t("aboutPara1")}</p>
+            <p className="text-beige whitespace-pre-wrap text-lg">{t("aboutPara2")}</p>
+            <p className="text-beige whitespace-pre-wrap text-lg">{t("aboutPara3")}</p>
         </div>
     );
 }
