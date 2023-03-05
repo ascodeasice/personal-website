d675faea93589ed619d565824d7f3aae73900c7d
ascodeasice <ab0988956087@gmail.com>
2023-03-05 12:33:53 +0800

    Click link opens new page in project section

--- src/components/ProjectPage/InfoRow.js
+++ src/components/ProjectPage/InfoRow.js
@@ -1,10 +1,10 @@
 const InfoRow = ({ src, link, text, inverted, className }) => {
     return (
         <div className={`grid items-center gap-x-4 grid-cols-[4rem_min-content] opacity-0 ${className}`}>
-            <a href={link}>
+            <a href={link} target="_blank">
                 <img src={src} className={`${inverted ? "invert" : ""} w-16 hover:scale-110 transition`} />
             </a>
-            <a href={link} className="text-custom-red whitespace-nowrap underline text-2xl hover:text-maroon">
+            <a href={link} target="_blank" className="text-custom-red whitespace-nowrap underline text-2xl hover:text-maroon">
                 {text}
             </a>
         </div>
