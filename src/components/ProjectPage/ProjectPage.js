7ac58ebb97e90b642763ac8459f7d3e50534d1b0
ascodeasice <ab0988956087@gmail.com>
2023-03-07 21:11:13 +0800

    Remove grid property in project page

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -14,7 +14,7 @@ const ProjectPage = ({ project, id, num, isLast }) => {
     }, []);
 
     return (
-        <div id={id ? id : ""} className="min-h-[90vh] bg-black grid justify-items-center pt-[10vh]">
+        <div id={id ? id : ""} className="min-h-[90vh] bg-black pt-[10vh]">
             <div className="w-[80vw] grid grid-cols-2 grid-rows-[repeat(5,min-content)] mt-[10vh] gap-x-4 max-project-break:gap-y-2">
                 <h2 className={`max-[450px]:text-2xl project-title-${num} text-beige col-span-2 text-4xl font-bold mb-4 opacity-0`}>{project.name}</h2>
                 <ProjectImage images={project.images} num={num} />
