3b0d7ed734ac97704f618805571c3e573f729685
ascodeasice <ab0988956087@gmail.com>
2023-03-07 20:54:45 +0800

    Add link to contact page

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -19,9 +19,9 @@ const ContactPage = () => {
             <div className="grid grid-rows-[min-content_1fr] gap-y-4 w-[70vw]">
                 <h1 className="max-[450px]:text-5xl contact-title text-beige font-bold text-6xl justify-self-start opacity-0">{t("Contact")}</h1>
                 <div className="contact-container bg-beige grid grid-rows-3 h-[60vh] p-8 opacity-0">
-                    <ContactRow src={GITHUB} href="#" text="Github" num={0} />
-                    <ContactRow src={LINKEDIN} href="#" text="LinkedIn" num={1} />
-                    <ContactRow src={MAIL} href="#" text="Email" num={2} />
+                    <ContactRow src={GITHUB} href="https://github.com/ascodeasice" text="Github" num={0} />
+                    <ContactRow src={LINKEDIN} href="https://www.linkedin.com/in/%E6%9F%8F%E5%87%B1-%E7%8E%8B-48b109226/" text="LinkedIn" num={1} />
+                    <ContactRow src={MAIL} href="mailto:ascodeasice@gmail.com" text="Email" num={2} />
                 </div>
             </div>
         </div>
--- src/components/ContactPage/ContactRow.js
+++ src/components/ContactPage/ContactRow.js
@@ -12,10 +12,10 @@ const ContactRow = ({ src, href, text, num }) => {
 
     return (
         <div className="grid grid-cols-[5rem_min-content] max-[450px]:grid-cols-[3rem_min-content] gap-x-4 items-center">
-            <a href={href}>
+            <a href={href} target="_blank">
                 <img src={src} className={`${iconClassName} w-20 h-20 hover:bg-maroon rounded transition-colors opacity-0 animation-delay-${400 + 100 * num} max-[450px]:w-12 max-[450px]:h-12`} />
             </a>
-            <a href={href} className={`${linkClassName} text-custom-red font-bold underline text-5xl hover:text-maroon  transition-colors opacity-0  animation-delay-${100 + 100 * num} max-[450px]:text-2xl`}>{text ?? "Link"}</a>
+            <a href={href} target="_blank" className={`${linkClassName} text-custom-red font-bold underline text-5xl hover:text-maroon  transition-colors opacity-0  animation-delay-${100 + 100 * num} max-[450px]:text-2xl`}>{text ?? "Link"}</a>
         </div>
     );
 }
