5e36f709ef9a665c7357c342d56419eb300586bc
ascodeasice <ab0988956087@gmail.com>
2023-03-07 21:01:12 +0800

    Change about text height to fit content

--- src/components/AboutPage/AboutText.js
+++ src/components/AboutPage/AboutText.js
@@ -12,10 +12,10 @@ const AboutText = () => {
     }, []);
 
     return (
-        <div className="bg-maroon rounded-lg py-4 px-4 max-w-[50vw] max-[420px]:max-w-[80vw] h-[70vh] grid grid-rows-[repeat(3,min-content)] gap-y-4">
-            <p className="aboutPara1 text-beige whitespace-pre-wrap min-[1100px]:text-lg min-[800px]:text-base min-[650px]:text-sm min-[320px]:text-xs opacity-0 animation-delay-100">{t("aboutPara1")}</p>
-            <p className="aboutPara2 text-beige whitespace-pre-wrap min-[1100px]:text-lg min-[800px]:text-base min-[650px]:text-sm min-[320px]:text-xs animation-delay-300 opacity-0">{t("aboutPara2")}</p>
-            <p className="aboutPara3 text-beige whitespace-pre-wrap min-[1100px]:text-lg min-[800px]:text-base min-[650px]:text-sm min-[320px]:text-xs animation-delay-500 opacity-0">{t("aboutPara3")}</p>
+        <div className="bg-maroon rounded-lg py-4 px-4 max-w-[50vw] max-[420px]:max-w-[80vw] h-fit grid grid-rows-[repeat(3,min-content)] gap-y-4">
+            <p className="aboutPara1 text-beige whitespace-pre-wrap min-[1100px]:text-lg min-[800px]:text-base min-[650px]:text-sm min-[320px]:text-[0.8rem] opacity-0 animation-delay-100">{t("aboutPara1")}</p>
+            <p className="aboutPara2 text-beige whitespace-pre-wrap min-[1100px]:text-lg min-[800px]:text-base min-[650px]:text-sm min-[320px]:text-[0.8rem] animation-delay-300 opacity-0">{t("aboutPara2")}</p>
+            <p className="aboutPara3 text-beige whitespace-pre-wrap min-[1100px]:text-lg min-[800px]:text-base min-[650px]:text-sm min-[320px]:text-[0.8rem] animation-delay-500 opacity-0">{t("aboutPara3")}</p>
         </div>
     );
 }
