ec6780c32569aa5243b88808cadc88f693123d99
ascodeasice <ab0988956087@gmail.com>
2023-03-07 21:14:18 +0800

    Center project page with absolute position

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -15,7 +15,7 @@ const ProjectPage = ({ project, id, num, isLast }) => {
 
     return (
         <div id={id ? id : ""} className="min-h-[90vh] bg-black pt-[10vh]">
-            <div className="w-[80vw] grid grid-cols-2 grid-rows-[repeat(5,min-content)] mt-[10vh] gap-x-4 max-project-break:gap-y-2">
+            <div className="w-[80vw] grid grid-cols-2 grid-rows-[repeat(5,min-content)] mt-[10vh] gap-x-4 max-project-break:gap-y-2 absolute inset-x-0 mx-auto">
                 <h2 className={`max-[450px]:text-2xl project-title-${num} text-beige col-span-2 text-4xl font-bold mb-4 opacity-0`}>{project.name}</h2>
                 <ProjectImage images={project.images} num={num} />
                 <ProjectInfo iconSrcArr={project.teches} liveLink={project.liveLink} githubLink={project.githubLink} num={num} />
