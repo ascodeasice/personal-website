66e47b973e8668831ba099025247f06e5b273fd1
Leo Wang <ab0988956087@gmail.com>
2023-12-05 00:05:16 +0800

    feat(i18n): show default language according to browser

--- src/components/Header/LanguageSwitch.js
+++ src/components/Header/LanguageSwitch.js
@@ -2,13 +2,13 @@ import { useState } from "react";
 import i18n from "i18next";
 
 const LanguageSwitch = () => {
-    const [language, setLanguage] = useState("en");
+    const [language, setLanguage] = useState(i18n.language);
 
     const handleClick = () => {
         if (language == "en") {
-            setLanguage("zh");
-            i18n.changeLanguage("zh")
-        } else if (language == "zh") {
+            setLanguage("zh-TW");
+            i18n.changeLanguage("zh-TW")
+        } else if (language == "zh-TW") {
             setLanguage("en");
             i18n.changeLanguage("en")
         } else {
@@ -20,7 +20,7 @@ const LanguageSwitch = () => {
 
     return (
         <button onClick={handleClick} className="rounded-full bg-black w-16 h-8 flex items-center px-1 animate-slide-up animation-delay-300 opacity-0" >
-            <div className={`rounded-full bg-beige w-6 h-6 transition ${language == "en" ? "switchOn" : " "}`}></div>
+            <div className={`rounded-full bg-beige w-6 h-6 transition ${language === "zh-TW" ? "" : "switchOn"}`}></div>
         </button>
     );
 }
--- src/i18n.js
+++ src/i18n.js
@@ -8,15 +8,23 @@ const resources = {
     en: {
         translation: en,
     },
-    zh: {
+    "zh-TW": {
         translation: zh_tw,
     },
 };
 
+let lang = navigator.language || navigator.userLanguage;
+
+const availableLanguages = ["en", "zh-TW"];
+
+if (!availableLanguages.includes(lang)) {
+    lang = "en"
+}
+
 i18n.use(initReactI18next).init({
     resources, // 會是所有翻譯資源
-    fallbackLng: "en", // 如果當前切換的語言沒有對應的翻譯則使用這個語言
-    lng: "en", // 預設語言
+    fallbackLng: lang, // 如果當前切換的語言沒有對應的翻譯則使用這個語言
+    lng: lang, // 預設語言
     interpolation: {
         // 是否要讓字詞 escaped 來防止 xss 攻擊，這裡因為 React.js 已經做了，就設成 false即可
         escapeValue: false,
