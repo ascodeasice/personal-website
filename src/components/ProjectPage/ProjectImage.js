02c8c2cecc61212fc9edc618fd9c7337da043798
Leo Wang <ab0988956087@gmail.com>
2023-12-05 00:19:49 +0800

    fix(ProjectImage): remove animation when switching image

--- src/components/ProjectPage/ProjectImage.js
+++ src/components/ProjectPage/ProjectImage.js
@@ -4,29 +4,21 @@ import scrollTrigger from '../../functions/scrollTrigger';
 
 const ProjectImage = ({ images, num }) => {
     const [imageIndex, setImageIndex] = useState(0);
-    const [switching, setSwitching] = useState(false);
-    const [isFirstRender, setIsFirstRender] = useState(true);
 
     const getNextImageIndex = () => {
         return imageIndex + 1 >= images.length ? 0 : imageIndex + 1;
     }
 
     const switchImage = () => {
-        setIsFirstRender(false);
-        setSwitching(false);
         setImageIndex(getNextImageIndex());
     }
 
     useEffect(() => {
-        if (isFirstRender) {
-            scrollTrigger(`.project-image-${num}`, `.project-image-${num}`, "animate-slide-up", 0.25, false)
-            return;
-        }
-        setSwitching(true);
-    }, [imageIndex]);
+        scrollTrigger(`.project-image-${num}`, `.project-image-${num}`, "animate-slide-up", 0.25, false)
+    }, []);
 
     return (
-        <img className={`max-project-break:col-span-2 max-project-break:row-span-1 project-image-${num} object-cover max-h-[50vh] row-span-3 cursor-pointer opacity-0 ${switching ? "animate-image-slide-right" : "animation-delay-200"}`} src={images[imageIndex]} onClick={switchImage} />
+        <img className={`max-project-break:col-span-2 max-project-break:row-span-1 project-image-${num} object-cover max-h-[50vh] row-span-3 cursor-pointer opacity-0`} src={images[imageIndex]} onClick={switchImage} />
     );
 }
 
